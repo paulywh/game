a80579620c5eea7b7794a96cd92c70b95be63821
whyan <710022315@qq.com>
2015-01-21 18:59:57 +0800

    add game

---
+++ nguidemo/Assets/scripts/gameset.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameType
+{
+	EASY,
+	HARD
+}
+
+public class gameset : MonoBehaviour 
+{
+	public float video = 1;
+
+	public GameType gt = GameType.EASY;
+
+	public bool isfull = false;
+
+	public TweenPosition initTP;
+	public TweenPosition optoverTP;
+
+	public UIInput input;
+
+	public void clickVideo()
+	{
+		video = UIProgressBar.current.value;
+	}
+
+	public void checkGt()
+	{
+		//print (UIPopupList.current.value);
+		switch (UIPopupList.current.value)
+		{
+			case "EASY" :
+				gt = GameType.EASY;
+			break;
+			case "HARD" :
+				gt = GameType.HARD;
+			break;
+		}
+	}
+
+	public void checkIsfull()
+	{
+		print ("cccc" + UIToggle.current.value);
+		if(UIToggle.current.value)
+		{
+			isfull = true;
+		}
+		else
+		{
+			isfull = false;
+		}
+	}
+
+	public void optClinc()
+	{
+		initTP.PlayForward ();
+		optoverTP.PlayForward ();
+	}
+
+	public void optOver()
+	{
+		Debug.Log ("*****************************" + input.value);
+
+		initTP.PlayReverse ();
+		optoverTP.PlayReverse ();
+	}
+}
---
+++ nguidemo/Assets/scripts/skill.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skill : MonoBehaviour {
+
+	public float coldtime = 2;
+	
+	private bool isstar = false;
+	
+	private UISprite sprite;
+	
+	void Awake()
+	{
+		sprite = transform.Find("onskill").GetComponent<UISprite>();
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.X) && !isstar)
+		{
+			sprite.fillAmount = 1;
+			isstar = true;
+		}
+		print ("________________________________");
+		if(isstar)
+		{
+			sprite.fillAmount -= (1f/coldtime) * Time.deltaTime;
+			if(sprite.fillAmount <= 0.05f)
+			{
+				isstar = false;
+				sprite.fillAmount = 0;
+			}
+		}
+	}
+}
